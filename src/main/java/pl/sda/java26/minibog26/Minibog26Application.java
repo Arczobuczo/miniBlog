0f254b476ce15bb558a4dce505fe2793fe6b5fa8
Arczo <arturboratynski@gmail.com>
2020-11-18 22:12:59 +0100

    creat mini bolg, controler, all

---
+++ src/main/java/pl/sda/java26/minibog26/Minibog26Application.java
@@ -0,0 +1,13 @@
+package pl.sda.java26.minibog26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minibog26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Minibog26Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/java26/minibog26/Minibog26ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.java26.minibog26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minibog26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
